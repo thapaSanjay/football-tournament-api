a9bc3c9d6bbc153e043e2d0c0aab6ea9678bfba1
Sanjay Thapa <sanjaya.thapa39@gmail.com>
2026-07-13 13:30:25 +1000

    Football Tournament Api Setup

---
+++ src/main/java/com/sanjay/footballtournamentapi/FootballTournamentApiApplication.java
@@ -0,0 +1,13 @@
+package com.sanjay.footballtournamentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTournamentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballTournamentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sanjay/footballtournamentapi/FootballTournamentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjay.footballtournamentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTournamentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
