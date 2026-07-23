a90b8c783e50aefea71893ffba054e792c744f36
Sanjay Thapa <sanjaya.thapa39@gmail.com>
2026-07-23 19:49:17 +1000

    Added Tournament Group controller end points

---
+++ src/main/java/com/sanjay/footballtournamentapi/controller/TournamentGroupController.java
@@ -0,0 +1,34 @@
+package com.sanjay.footballtournamentapi.controller;
+
+import com.sanjay.footballtournamentapi.model.TournamentGroup;
+import com.sanjay.footballtournamentapi.service.TournamentGroupService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/groups")
+public class TournamentGroupController {
+
+    private final TournamentGroupService tournamentGroupService;
+
+    public TournamentGroupController( TournamentGroupService tournamentGroupService ) {
+        this.tournamentGroupService = tournamentGroupService;
+    }
+
+    @PostMapping("/tournament/{tournamentId}")
+    public TournamentGroup addGroupToTournament(@PathVariable int tournamentId, @RequestBody TournamentGroup tournamentGroup) {
+        return tournamentGroupService.addGroupToTournament(tournamentId, tournamentGroup);
+    }
+
+    @GetMapping
+    public List<TournamentGroup> getAllGroups() {
+        return tournamentGroupService.getAllGroups();
+    }
+
+    @GetMapping("/tournament/{tournamentId}")
+    public List<TournamentGroup> getGroupsByTournamentId(@PathVariable int tournamentId) {
+        return tournamentGroupService.getGroupsByTournamentId(tournamentId);
+    }
+
+}
