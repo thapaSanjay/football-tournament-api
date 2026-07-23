022d9011bfab2fe72f75730e38113e61ecf19121
Sanjay Thapa <sanjaya.thapa39@gmail.com>
2026-07-23 20:44:11 +1000

    Add Football Match entity

---
+++ src/main/java/com/sanjay/footballtournamentapi/model/FootballMatch.java
@@ -0,0 +1,94 @@
+package com.sanjay.footballtournamentapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "matches")
+public class FootballMatch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)10
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "home_team_id")
+    private Team homeTeam;
+
+    @ManyToOne
+    @JoinColumn(name = "away_team_id")
+    private Team awayTeam;
+
+    private int homeScore;
+    private int awayScore;
+
+    private String round;
+    private String status;
+
+    public FootballMatch() {
+    }
+
+    public FootballMatch(Team homeTeam, Team awayTeam, int homeScore, int awayScore, String round, String status) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
+        this.round = round;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Team getHomeTeam() {
+        return homeTeam;
+    }
+
+    public Team getAwayTeam() {
+        return awayTeam;
+    }
+
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public int getAwayScore() {
+        return awayScore;
+    }
+
+    public String getRound() {
+        return round;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setHomeTeam(Team homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public void setAwayTeam(Team awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public void setHomeScore(int homeScore) {
+        this.homeScore = homeScore;
+    }
+
+    public void setAwayScore(int awayScore) {
+        this.awayScore = awayScore;
+    }
+
+    public void setRound(String round) {
+        this.round = round;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
